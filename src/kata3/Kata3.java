338a3c368d3c24583c6319c975d3dd97ef29cb3a
ignaciomarinreyes <ignaciomarinreyes@192.168.0.15>
2019-12-31 16:43:27 +0000

    Kata3 version2

--- src/kata3/Kata3.java
+++ src/kata3/Kata3.java
@@ -3,9 +3,21 @@ package kata3;
 public class Kata3 {
 
     public static void main(String[] args) {
+        Histogram<String> histogram = new Histogram<>();
+        histogram.increment("gmail.com");
+        histogram.increment("gmail.com");
+        histogram.increment("gmail.com");
+        histogram.increment("ulpgc.es");
+        histogram.increment("ulpgc.es");
+        histogram.increment("ulpgc.es");
+        histogram.increment("ulpgc.es");
+        histogram.increment("gmail.com");
+        histogram.increment("hotmail.com");
+        histogram.increment("hotmail.es");
+        histogram.increment("ull.es");
+        histogram.increment("hotmail.com");
+        histogram.increment("ull.es");
 
-        HistogramDisplay histo = new HistogramDisplay("JFreeChart");
-        histo.execute();
+        new HistogramDisplay(histogram).execute();
     }
-
 }
